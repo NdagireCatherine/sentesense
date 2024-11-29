9574aabc3e9713921f9391ab3cde2100fd4ab185
anitahkembabazi <kembabaziritah0@gmail.com>
2024-11-29 17:56:33 +0300

    commit

--- src/Pages/Expenses.js
+++ src/Pages/Expenses.js
@@ -1,9 +1,13 @@
 import Navbar from "../Components/Navbar"
-
+import ExpensesComponent from "../Components/Expenses";
 export default function Expenses() {
     return (
       <div>
         <Navbar/>
+        <ExpensesComponent/>
+
+    
       </div>
     );
   }
+  
