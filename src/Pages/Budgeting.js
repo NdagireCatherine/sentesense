338cef0e93440b815ebe21dcb4c18ae5b928866d
Ndagire Catherine <tronellahn@gmail.com>
2024-11-29 17:55:24 +0300

    commit

--- src/Pages/Budgeting.js
+++ src/Pages/Budgeting.js
@@ -1,11 +1,12 @@
+import React, { useState } from "react";
 import Navbar from "../Components/Navbar"
+import Budgeting from "../Components/Budgeting";
 
-
-export default function Budgeting() {
+export default function Expenses() {
     return (
       <div>
         <Navbar/>
-        
+        <Budgeting/>
       </div>
     );
   }
