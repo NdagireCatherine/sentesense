f516918cc2e6e2a99f041c4f56a35fa71d25fe4d
Ndagire Catherine <tronellahn@gmail.com>
2024-11-30 00:53:01 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import Budgeting from './Pages/Budgeting'
 import Reports from './Pages/Reports'
 import SignIn from './Pages/SignIn'
 import SignUp from './Pages/SignUp'
+import Dashboard from './Pages/Dashoard'
 
 export default function App(){
   return(
     <div>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home/>}/>
+          <Route index element={<Dashboard/>}/>
           <Route path="/Home" element={<Home/>}/>
           <Route path="/Income" element={<Income/>}/>
           <Route path="/Expenses" element={<Expenses/>}/>
@@ -20,6 +21,7 @@ export default function App(){
           <Route path="/Reports" element={<Reports/>}/>
           <Route path="/SignIn" element={<SignIn/>}/>
           <Route path="/SignUp" element={<SignUp/>}/>
+          <Route path="/Dashboard" element={<Dashboard/>}/>
         </Routes>
       </BrowserRouter>
     </div>
